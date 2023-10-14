b1ca0f58a8b11e2ba60e49ae71d70330dd302dc1
Maria Vélez <29411663+velezdev@users.noreply.github.com>
2023-10-13 17:39:38 -0700

    write a large file data chunk

---
+++ stream.js
@@ -0,0 +1,13 @@
+// if we have large files, it can be more efficient to not move everything at once
+const fs = require('fs')
+
+// can use path module but hardcoding it for ease
+const rs = fs.createReadStream('./files/hipster_lorem.txt', {encoding: 'utf8'})
+
+// tell it what file to write it to 
+const ws = fs.createWriteStream('./files/new_hipster_lorem.txt') 
+
+// listen for the data coming into the stream
+rs.on('data', (dataChunk) => {
+	ws.write(dataChunk) // this will generate the new_hipster_lorem.txt file 
+})
