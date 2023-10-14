8b80f6c543a9d88061b51628ab8dd8837736998d
Maria Vélez <29411663+velezdev@users.noreply.github.com>
2023-10-13 17:41:14 -0700

    piping instead

--- stream.js
+++ stream.js
@@ -8,6 +8,9 @@ const rs = fs.createReadStream('./files/hipster_lorem.txt', {encoding: 'utf8'})
 const ws = fs.createWriteStream('./files/new_hipster_lorem.txt') 
 
 // listen for the data coming into the stream
-rs.on('data', (dataChunk) => {
-	ws.write(dataChunk) // this will generate the new_hipster_lorem.txt file 
-})
+// rs.on('data', (dataChunk) => {
+// 	ws.write(dataChunk) // this will generate the new_hipster_lorem.txt file 
+// })
+
+// a better way to do the above; piping
+rs.pipe(ws)
