b7fa935118c6185b78419c7265deb6f059eb8a4b
Maria Vélez <29411663+velezdev@users.noreply.github.com>
2023-10-13 20:48:32 -0700

    created a new directory after checking if it exists

--- dir.js
+++ dir.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 
+// if the directory doesn't exist, do what's inside the 'if' statement
 // create a directory
-fs.mkdir('./new', (err) => {
-	if (err) throw err;
-	console.log('i created a directory!')
-})
+if (!fs.existsSync('./new')) {
+	fs.mkdir('./new', (err) => {
+		if (err) throw err;
+		console.log('i created a directory!')
+	})
+}
--- stream.js
+++ stream.js
@@ -1,16 +1,18 @@
 // if we have large files, it can be more efficient to not move everything at once
-const fs = require('fs')
+const fs = require("fs");
 
 // can use path module but hardcoding it for ease
-const rs = fs.createReadStream('./files/hipster_lorem.txt', {encoding: 'utf8'})
+const rs = fs.createReadStream("./files/hipster_lorem.txt", {
+  encoding: "utf8",
+});
 
-// tell it what file to write it to 
-const ws = fs.createWriteStream('./files/new_hipster_lorem.txt') 
+// tell it what file to write it to
+const ws = fs.createWriteStream("./files/new_hipster_lorem.txt");
 
 // listen for the data coming into the stream
 // rs.on('data', (dataChunk) => {
-// 	ws.write(dataChunk) // this will generate the new_hipster_lorem.txt file 
+// 	ws.write(dataChunk) // this will generate the new_hipster_lorem.txt file
 // })
 
 // a better way to do the above; piping
-rs.pipe(ws)
+rs.pipe(ws);
